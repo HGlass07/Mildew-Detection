61406ab040c4ffa6ad56a9c49524f4508c4409e2
Henry <hglasspool@googlemail.com>
2024-08-27 16:36:41 +0000

    updated multipage page icon

--- app_pages/multipage.py
+++ app_pages/multipage.py
@@ -8,7 +8,7 @@ class MultiPage:
 
         st.set_page_config(
             page_title=self.app_name,
-            page_icon="")
+            page_icon="🌳")
 
     def add_page(self, title, func) -> None:
         self.pages.append({"title": title, "function": func})
