c132f803d6842de95287a9263a57bd73b0424e9e
Henry <hglasspool@googlemail.com>
2024-08-28 14:12:40 +0000

    added ML evaluation details to ML performance page

--- app_pages/model_performance.py
+++ app_pages/model_performance.py
@@ -1,5 +1,13 @@
 import streamlit as st
 import matplotlib.pyplot as plt
+import pandas as pd
+import joblib
+
+def load_pkl_file(file_path):
+    return joblib.load(filename=file_path)
+
+def load_test_evaluation(version):
+    return load_pkl_file(f'outputs/{version}/model_evaluation.pkl')
 
 def model_performance_body():
     version = 'v1'
@@ -19,3 +27,6 @@ def model_performance_body():
     model_loss = plt.imread(f"outputs/{version}/loss_plot.png")
     st.image(model_loss, caption='Model Training Losses')
     st.write("---")
+
+    # Load model evaluation metrics from the pickle file
+    st.dataframe(pd.DataFrame(load_test_evaluation(version), index=['Loss', 'Accuracy']))
