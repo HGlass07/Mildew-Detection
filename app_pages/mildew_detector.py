dbaa3f2b9f3a5ff8eadd492d5696e17254382916
Henry <hglasspool@googlemail.com>
2024-08-28 10:04:26 +0000

    loaded model and created img widget for prediction

--- app_pages/mildew_detector.py
+++ app_pages/mildew_detector.py
@@ -1,8 +1,42 @@
 import streamlit as st
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array
 from PIL import Image
 
+# load and preprocess an image
+def load_and_prep_image(image, target_size=(100, 100)):
+    image = image.resize(target_size)
+    image = img_to_array(image)
+    image = image / 255.0 
+    image = np.expand_dims(image, axis=0)
+    return image
+
+# predict on uploaded images
+def predict_on_images(images, model):
+    predictions = []
+    for image in images:
+        img_array = load_and_prep_image(image)
+        prediction = model.predict(img_array)
+        predictions.append(prediction[0][0])
+    return predictions
+
 def mildew_detector_body():
-    st.write("This is page 3")
+    st.title("Image Uploader")
 
-
+    # load model
+    model = load_model("outputs/v1/mildew_detector_model.h5")
     
+    uploaded_files = st.file_uploader("Choose images...", type=["jpg", "png", "jpeg"], accept_multiple_files=True)
+    
+    if uploaded_files:
+        images = [Image.open(file) for file in uploaded_files]
+        
+        st.image(images, caption=[file.name for file in uploaded_files], use_column_width=True)
+        
+        predictions = predict_on_images(images, model)
+        
+        for i, prediction in enumerate(predictions):
+            label = "Healthy" if prediction < 0.5 else "Infected"
+            probability = 1 - prediction if label == "Healthy" else prediction
+            st.write(f"Image {uploaded_files[i].name} is predicted to be: **{label}** with a probability of **{probability:.4f}**")
