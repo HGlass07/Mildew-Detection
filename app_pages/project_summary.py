2cdb2c631ef7fa46d1258f35c192bf079dd4da2a
Henry <hglasspool@googlemail.com>
2024-08-29 14:29:26 +0000

    add background and hypothesis content to dashboard

--- app_pages/project_summary.py
+++ app_pages/project_summary.py
@@ -2,4 +2,28 @@ import streamlit as st
 
 
 def project_summary_body():
-    st.write("This is page 1")
+
+    st.write("### Project Background")
+    st.info(
+        f"The cherry plantation crop from Farmy & Foods is facing a challenge " 
+        f"where their cherry plantations have been presenting powdery mildew. " 
+        f"Currently, the process is manual verification if a given cherry tree "
+        f"contains powdery mildew. To save time, the IT team has suggested "
+        f"that a machine learning system could be used to detect the pressence "
+        f"of mildew on leaf images\n\n"
+        f"**Business Requirements**\n\n"
+        f"- The client is interested in conducting a study to visually differentiate "
+        "a healthy cherry leaf from one with powedery mildew.\n"
+        f"- The client is interested in predicting if a cherry leaf is healthy or "
+        f" contains powdery mildew.")
+
+    st.write("### Hypothesis")
+    st.info(
+        f"As the leaves of cherry trees can be manually differentiated by the " 
+        f"visible presence of powdery mildew, it is believed that a classification "
+        f"machine learning model can expedite this process by instantly discerning " 
+        f"if mildew is present on a leaf. This would be achieved through an "
+        f"average image study, with the accuracy of the model informing the "
+        f"validity of the hypothesis. ")
+
+        
